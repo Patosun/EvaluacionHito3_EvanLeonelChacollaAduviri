577f199af6229ccb3d2f9d1718b2b2945620894a
Evan <battu.mix@gmail.com>
2026-05-08 13:59:46 -0400

    Estructura del proyecto

---
+++ src/main/java/com/hito3/Hito3Application.java
@@ -0,0 +1,12 @@
+package com.hito3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hito3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hito3Application.class, args);
+    }
+}
